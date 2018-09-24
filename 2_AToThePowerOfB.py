e2e054c8f95ba22525291e90b8a781e9f6b5d204
Yulwei <gonzacalleja@gmail.com>
2018-09-24 19:02:56 +0200

    final

--- 2_AToThePowerOfB.py
+++ 2_AToThePowerOfB.py
@@ -1,25 +1,26 @@
 
 def multiply(x, y):
-    for
+    result = 0
+    for i in range(y):
+        result += x
+
+    return result
+
+
+def power(x, y):
+    result = x
+
+    if y == 0:
+        result = 0
+    else:
+        for i in range(y-1):
+            result = multiply(result, x)
+
+    return result
+
 
 print("This program will output the answer to num1 ^ num2 \n")
 number_1 = int(input("Please introduce the first number: "))
 number_2 = int(input("Please introduce the second number: "))
 
-## 2 * 4 -> 2+2+2+2
-## 2**3 -> 2*2*2
-##         2+2+2+2
-##2 ^ 4 - ((2*2)*2)*2 -> ((2+2)+(2+2))
-result = 0
-for c in range(number_2):
-    result += number_1
-    print(result)
-
-
-print(result)
-
-def rise(num1, num2):
-    for c in range(num2):
-        result
-
-
+print("The result of", number_1, "^", number_2,"is: ", power(number_1, number_2))
