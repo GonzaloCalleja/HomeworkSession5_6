509d9abc6fbfdc79c33ca93d314313b2d60f30fe
Yulwei <gonzacalleja@gmail.com>
2018-09-24 19:05:47 +0200

    comment

--- 1_MultiplicationTable.py
+++ 1_MultiplicationTable.py
@@ -1,4 +1,7 @@
 
+# It doesn't repeat results for the commutative property (a*b = b*a)
+# It does repeat resutlts for the differene factors of a number (4*4 = 16 = 2*8)
+
 for x in range(1, 11):
     for y in range(x, 11):
         print(x, "*", y, "=", x*y)
