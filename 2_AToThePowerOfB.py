444fc068599946e69efc1499d83238546a5f602f
Yulwei <gonzacalleja@gmail.com>
2018-09-25 11:13:58 +0200

    several solutions

--- 2_AToThePowerOfB.py
+++ 2_AToThePowerOfB.py
@@ -1,26 +1,56 @@
 
 def multiply(x, y):
-    result = 0
+    result1 = 0
     for i in range(y):
-        result += x
+        result1 += x
 
-    return result
+    return result1
 
 
 def power(x, y):
-    result = x
+    result1 = x
 
     if y == 0:
-        result = 0
+        result1 = 0
     else:
         for i in range(y-1):
-            result = multiply(result, x)
+            result1 = multiply(result1, x)
 
-    return result
+    return result1
 
 
 print("This program will output the answer to num1 ^ num2 \n")
-number_1 = int(input("Please introduce the first number: "))
-number_2 = int(input("Please introduce the second number: "))
+try:
+    number_1 = int(input("Please introduce the first number: "))
+    number_2 = int(input("Please introduce the second number: "))
+except:
+    print("Please input a number")
 
 print("The result of", number_1, "^", number_2,"is: ", power(number_1, number_2))
+
+
+# Without using any functions
+result = number_1
+for i in range(number_2-1):
+    temp_result = 0
+    for c in range(number_1):
+        temp_result += result
+
+    result = temp_result
+
+print(result)
+
+# print(list(range(10,1,-1)))
+
+a = 5
+b = 4
+
+power = 1
+
+for i in range(b):
+    result = 0
+    for j in range(a):
+        result += power
+    power = result
+
+print(power)
